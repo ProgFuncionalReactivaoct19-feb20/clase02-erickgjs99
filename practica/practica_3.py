10578eaffab298320ccf0a4dc504360955589de8
erickgjs99 <erickjs99lol@gmail.com>
2019-10-08 11:27:24 -0500

    ejercicio_3 de la practica

---
+++ practica/practica_3.py
@@ -0,0 +1,8 @@
+"""
+    autor: @erickgjs99
+    file: practica_3.py
+    Desarrollar la función; debe presentar en pantalla para el ejemplo 21
+"""
+sum = lambda num, num_2: num + num_2
+
+print(sum(10, 11)) 
