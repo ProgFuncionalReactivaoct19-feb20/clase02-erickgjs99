87877d1d8a1fdcd1006191c94bae4a4d60a9d4cf
erickgjs99 <erickjs99lol@gmail.com>
2019-10-08 11:22:48 -0500

    ejercicio 2 practica

--- practica/practica_2.py
+++ practica/practica_2.py
@@ -1,5 +1,9 @@
 """
     autor: @erickgjs99
     file: practica_2.py
-    
+    Desarrollar una función anónima que permita retornar la siguiente salida, ejemplo:
+    CUENCA capital de AZUAY
 """
+mensaje = lambda cad, cad_2: "%s Capital de %s" % (cad.upper(), cad_2.upper())
+
+print(mensaje("Cuenca", "Azuay"))
